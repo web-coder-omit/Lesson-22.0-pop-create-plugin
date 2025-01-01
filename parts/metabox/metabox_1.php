c6356769e5176593da9b5d53b46e8fdd26d88946
Your Name <omit8998@gmail.com>
2025-01-01 23:27:53 +0600

    Update my code

---
+++ parts/metabox/metabox_1.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Title: My Demo Metabox
+Post Type: popups
+*/
+    piklist('field', array(
+        'type' => 'text',
+        'field' => 'demo_text',
+        'label' => 'Text Box',
+        'value' => 'Default text',
+        'help' => 'This is help text.'
+
+      ));
+    
+
+?>
--- popup.php
+++ popup.php
@@ -9,6 +9,7 @@
  * License:     GPL-2.0+
  * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
  * text-domain:pop
+ * Plugin Type: Piklist
  */
 function plugin_file_function()
 {
@@ -16,12 +17,12 @@ function plugin_file_function()
 }
 add_action('plugins_loaded', 'plugin_file_function');
 
-function demowidget_admin_enqueue_scripts()
-{
-    wp_enqueue_style("demowidget-admin-ui-css", plugin_dir_url(__FILE__) . "asset/style.css");
-    wp_enqueue_script("iamges_popup", plugin_dir_url(__FILE__) . "asset/main.js");
-}
-add_action("admin_enqueue_scripts", "demowidget_admin_enqueue_scripts");
+// function demowidget_admin_enqueue_scripts()
+// {
+//     wp_enqueue_style("demowidget-admin-ui-css", plugin_dir_url(__FILE__) . "asset/style.css");
+//     wp_enqueue_script("iamges_popup", plugin_dir_url(__FILE__) . "asset/main.js");
+// }
+// add_action("admin_enqueue_scripts", "demowidget_admin_enqueue_scripts");
 
 
 
@@ -81,6 +82,65 @@ function create_popup_cpt() {
 
 }
 add_action( 'init', 'create_popup_cpt', 0 );
+
+// Register Custom Post Type popup
+// function create_popup_cpt() {
+
+// 	$labels = array(
+// 		'name' => _x( 'popups', 'Post Type General Name', 'pop' ),
+// 		'singular_name' => _x( 'popup', 'Post Type Singular Name', 'pop' ),
+// 		'menu_name' => _x( 'popups', 'Admin Menu text', 'pop' ),
+// 		'name_admin_bar' => _x( 'popup', 'Add New on Toolbar', 'pop' ),
+// 		'archives' => __( 'popup Archives', 'pop' ),
+// 		'attributes' => __( 'popup Attributes', 'pop' ),
+// 		'parent_item_colon' => __( 'Parent popup:', 'pop' ),
+// 		'all_items' => __( 'All popups', 'pop' ),
+// 		'add_new_item' => __( 'Add New popup', 'pop' ),
+// 		'add_new' => __( 'Add New', 'pop' ),
+// 		'new_item' => __( 'New popup', 'pop' ),
+// 		'edit_item' => __( 'Edit popup', 'pop' ),
+// 		'update_item' => __( 'Update popup', 'pop' ),
+// 		'view_item' => __( 'View popup', 'pop' ),
+// 		'view_items' => __( 'View popups', 'pop' ),
+// 		'search_items' => __( 'Search popup', 'pop' ),
+// 		'not_found' => __( 'Not found', 'pop' ),
+// 		'not_found_in_trash' => __( 'Not found in Trash', 'pop' ),
+// 		'featured_image' => __( 'Featured Image', 'pop' ),
+// 		'set_featured_image' => __( 'Set featured image', 'pop' ),
+// 		'remove_featured_image' => __( 'Remove featured image', 'pop' ),
+// 		'use_featured_image' => __( 'Use as featured image', 'pop' ),
+// 		'insert_into_item' => __( 'Insert into popup', 'pop' ),
+// 		'uploaded_to_this_item' => __( 'Uploaded to this popup', 'pop' ),
+// 		'items_list' => __( 'popups list', 'pop' ),
+// 		'items_list_navigation' => __( 'popups list navigation', 'pop' ),
+// 		'filter_items_list' => __( 'Filter popups list', 'pop' ),
+// 	);
+// 	$args = array(
+// 		'label' => __( 'popup', 'pop' ),
+// 		'description' => __( '', 'pop' ),
+// 		'labels' => $labels,
+// 		'menu_icon' => '',
+// 		'supports' => array(),
+// 		'taxonomies' => array(),
+// 		'public' => false,
+// 		'show_ui' => true,
+// 		'show_in_menu' => false,
+// 		'menu_position' => 5,
+// 		'show_in_admin_bar' => true,
+// 		'show_in_nav_menus' => true,
+// 		'can_export' => true,
+// 		'has_archive' => true,
+// 		'hierarchical' => false,
+// 		'exclude_from_search' => false,
+// 		'show_in_rest' => true,
+// 		'publicly_queryable' => true,
+// 		'capability_type' => 'post',
+// 	);
+// 	register_post_type( 'popup', $args );
+
+// }
+// add_action( 'init', 'create_popup_cpt', 0 );
+
 
 
 // function remove_editor_meta_box() {
